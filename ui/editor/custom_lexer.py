d0e1651f99a82c8f19fd619b62aa7c631cf7c30b
hustlei <hustlei@sina.cn>
2019-09-01 13:23:32 +0800

    add qss keywords to autocompleter

--- ui/editor/custom_lexer.py
+++ ui/editor/custom_lexer.py
@@ -1,6 +1,6 @@
 import re
 
-from PyQt5.Qsci import QsciScintilla, QsciLexerCustom
+from PyQt5.Qsci import QsciScintilla, QsciLexerCustom,QsciAPIs
 from PyQt5.QtGui import QColor, QFont
 
 
@@ -34,6 +34,7 @@ class QsciLexerQSS(QsciLexerCustom):
         super().__init__(codeEditor)
         self.__editor=codeEditor
         self.setDefaultStyle()
+        self.setQssAutocomplete()
 
     def setDefaultStyle(self):
         # Default text settings
@@ -317,6 +318,69 @@ class QsciLexerQSS(QsciLexerCustom):
 
     def isAWordChar(self, ch):
         return ch >= 0x80 or ch.isalnum() or ch == "-" or ch == "_"
+
+    def setQssAutocomplete(self):
+        api=QsciAPIs(self)
+        widgets=("QAbstractScrollArea", "QCheckBox", "QColumnView", "QComboBox", "QDateEdit", "QDateTimeEdit",
+                "QDialog", "QDialogButtonBox", "QDockWidget", "QDoubleSpinBox", "QFrame", "QGroupBox",
+                "QHeaderView", "QLabel", "QLineEdit", "QListView", "QListWidget", "QMainWindow", "QMenu",
+                "QMenuBar", "QMessageBox", "QProgressBar", "QPushButton", "QRadioButton", "QScrollBar",
+                "QSizeGrip", "QSlider", "QSpinBox", "QSplitter", "QStatusBar", "QTabBar", "QTabWidget",
+                "QTableView", "QTableWidget", "QTextEdit", "QTimeEdit", "QToolBar", "QToolButton", "QToolBox",
+                "QToolTip", "QTreeView", "QTreeWidget", "QWidget")
+        properties=("alternate-background-color", "background", "background-color", "background-image" ,
+                    "background-repeat", "background-position", "background-attachment", "background-clip" ,
+                    "background-origin", "border", "border-top", "border-right", "border-bottom", "border-left",
+                    "border-color", "border-top-color", "border-right-color", "border-bottom-color",
+                    "border-left-color", "border-image", "border-radius", "border-top-left-radius",
+                    "border-top-right-radius", "border-bottom-right-radius", "border-bottom-left-radius",
+                    "border-style", "border-top-style", "border-right-style", "border-bottom-style",
+                    "border-left-style", "border-width", "border-top-width", "border-right-width",
+                    "border-bottom-width", "border-left-width", "bottom", "button-layout", "color",
+                    "dialogbuttonbox-buttons-have-icons", "font", "font-family", "font-size", "font-style",
+                    "font-weight", "gridline-color", "height", "icon-size", "image", "image-position", "left",
+                    "lineedit-password-character", "lineedit-password-mask-delay", "margin", "margin-top",
+                    "margin-right", "margin-bottom", "margin-left", "max-height", "max-width",
+                    "messagebox-text-interaction-flags", "min-height", "min-width", "opacity*", "outline",
+                    "outline-color", "outline-offset", "outline-style", "outline-radius", "outline-bottom-left-radius",
+                    "outline-bottom-right-radius", "outline-top-left-radius", "outline-top-right-radius",
+                    "padding", "padding-top", "padding-right", "padding-bottom", "padding-left",
+                    "paint-alternating-row-colors-for-empty-area", "position", "right", "selection-background-color",
+                    "selection-color", "show-decoration-selected", "spacing", "subcontrol-origin",
+                    "subcontrol-position", "titlebar-show-tooltips-on-buttons", "widget-animation-duration",
+                    "text-align", "text-decoration", "top", "width")
+        subcontrols0=("::add-line", "::add-page", "::branch", "::chunk", "::close-button", "::corner", "::down-arrow",
+                     "::down-button", "::drop-down", "::float-button", "::groove", "::indicator", "::handle", "::icon",
+                     "::item", "::left-arrow", "::left-corner", "::menu-arrow", "::menu-button", "::menu-indicator",
+                     "::right-arrow", "::pane", "::right-corner", "::scroller", "::section", "::separator",
+                     "::sub-line", "::sub-page", "::tab", "::tab-bar", "::tear", "::tearoff", "::text", "::title",
+                     "::up-arrow", "::up-button")
+        pseudostates0=(":active", ":adjoins-item", ":alternate", ":bottom", ":checked", ":closable", ":closed",
+                      ":default", ":disabled", ":editable", ":edit-focus", ":enabled", ":exclusive", ":first",
+                      ":flat", ":floatable", ":focus", ":has-children", ":has-siblings", ":horizontal", ":hover",
+                      ":indeterminate", ":last", ":left", ":maximized", ":middle", ":minimized", ":movable",
+                      ":no-frame", ":non-exclusive", ":off", ":on", ":only-one", ":open", ":next-selected",
+                      ":pressed", ":previous-selected", ":read-only", ":right", ":selected", ":top", ":unchecked",
+                      ":vertical", ":window")
+        subcontrols=("add-line", "add-page", "branch", "chunk", "close-button", "corner", "down-arrow",
+                     "down-button", "drop-down", "float-button", "groove", "indicator", "handle", "icon",
+                     "item", "left-arrow", "left-corner", "menu-arrow", "menu-button", "menu-indicator",
+                     "right-arrow", "pane", "right-corner", "scroller", "section", "separator",
+                     "sub-line", "sub-page", "tab", "tab-bar", "tear", "tearoff", "text", "title",
+                     "up-arrow", "up-button")
+        pseudostates=("active", "adjoins-item", "alternate", "bottom", "checked", "closable", "closed",
+                      "default", "disabled", "editable", "edit-focus", "enabled", "exclusive", "first",
+                      "flat", "floatable", "focus", "has-children", "has-siblings", "horizontal", "hover",
+                      "indeterminate", "last", "left", "maximized", "middle", "minimized", "movable",
+                      "no-frame", "non-exclusive", "off", "on", "only-one", "open", "next-selected",
+                      "pressed", "previous-selected", "read-only", "right", "selected", "top", "unchecked",
+                      "vertical", "window")
+        kwset=(widgets,properties,subcontrols,pseudostates)
+        for ks in kwset:
+            for k in ks:
+                api.add(k)
+        api.prepare()
+
 
 if __name__ == '__main__':
     from PyQt5.QtWidgets import *
