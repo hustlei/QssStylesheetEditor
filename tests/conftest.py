7f2688be4369fbc61b243874f2e030b4d04ab5db
hustlei <hustlei@sina.cn>
2019-11-11 00:56:23 +0800

    add test

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,6 +7,10 @@ Copyright (c) 2019 lileilei <hustlei@sina.cn>
 from pytest import fixture
 from app import App
 
+# dir or module
+collect_ignore = ["setup.py", "chardet", "flow_layout.py"]
+collect_ignore_glob = ["*_v0.py", "*.old.py", "*_bak.py"]
+
 
 @fixture(scope="session")
 def windows():
--- tests/test_mainwin.py
+++ tests/test_mainwin.py
@@ -4,6 +4,11 @@
 Copyright (c) 2019 lileilei <hustlei@sina.cn>
 """
 
+# from PyQt5.QtCore import Qt
+from unittest import mock
+
+from PyQt5.QtWidgets import QApplication
+
 
 def test_confDialog(qtbot, windows):
     windows["main"].confDialog.show()
@@ -15,6 +20,35 @@ def test_findDialog(qtbot, windows):
     qtbot.waitForWindowShown(windows["main"].editor.searchDialog)
 
 
+def test_theme(windows):
+    win = windows["main"]
+    win.actions["DisableQss"].setChecked(True)
+    win.themeCombo.setCurrentIndex(win.themeCombo.maxCount() - 1)
+    assert win.actions["DisableQss"].isChecked()
+
+
+# def test_clrpic(qtbot, windows, monkeypatch):
+#     win=windows["main"]
+#     qtbot.mouseClick(win.clrBtnDict["text"], Qt.LeftButton)
+#     qtbot.wait(1)
+#     # qtbot.keyPress(win.focusWidget(), Qt.Key_Return, delay=100)
+#     # qtbot.keyPress(win.focusWidget(), Qt.Key_Enter)
+#     assert win.clrBtnDict["text"].text() == "#222222"
+
+
+def test_file(windows, tmpdir):
+    win = windows["main"]
+    win.new()
+    f = tmpdir.join("new.qsst").ensure()
+    # f.write("") # create file new.qsst
+    win.file = str(f)
+    win.save()
+    assert not win.editor.text()
+
+
 def test_mainwin(qtbot, windows):
     windows["main"].show()
-    qtbot.waitForWindowShown(windows["main"])
+    # qtbot.waitForWindowShown(windows["main"])
+    with mock.patch.object(QApplication, "exit"):
+        windows["main"].close()
+        assert QApplication.exit.call_count == 0
