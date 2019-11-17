1adfd123513cfb8ecec642e9fb71fbc81f87f26f
hustlei <hustlei@sina.cn>
2019-11-17 20:39:26 +0800

    using QThread instead of Thread

--- tests/test_colorpic.py
+++ tests/test_colorpic.py
@@ -4,7 +4,7 @@
 Copyright (c) 2019 lileilei <hustlei@sina.cn>
 """
 
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QThread
 
 
 def test_fileop_and_clrpic(qapp, qtbot, mainwin, tmpdir):
@@ -22,24 +22,28 @@ def test_fileop_and_clrpic(qapp, qtbot, mainwin, tmpdir):
     mainwin.newFromTemplate()
     mainwin.editor.setModified(False)
 
-    def closeclrdialog():
-        while not qapp.activeModalWidget():
-            qtbot.wait(100)
-        qtbot.wait(200)
-        dial = qapp.activeModalWidget()
-        qapp.processEvents()
-        # dial.setCurrentColor(QColor(255,0,0)) # must in ui thread
-        # qapp.processEvents()
-        # dial.done(0)    # dial.close()
-        qtbot.keyPress(dial, Qt.Key_Enter)
-        # qtbot.keyPress(qapp.focusWidget(), Qt.Key_Return, delay=50)
-        # os._exit(0)  # it will exit test, not exit thread
+    class DialogCloseThread(QThread):
+        def __init__(self, parent=None):
+            super().__init__(parent)
+            
+        def run(self):
+            print("aaa")
+            while not qapp.activeModalWidget():
+                qtbot.wait(100)
+            qtbot.wait(200)
+            dial = qapp.activeModalWidget()
+            qapp.processEvents()
+            # dial.setCurrentColor(QColor(255,0,0)) # must in ui thread
+            # qapp.processEvents()
+            # dial.done(0)    # dial.close()
+            qtbot.keyPress(dial, Qt.Key_Enter)
+            # qtbot.keyPress(qapp.focusWidget(), Qt.Key_Return, delay=50)
+            # os._exit(0)  # it will exit test, not exit thread
 
-    from threading import Thread
-    t1 = Thread(target=closeclrdialog)
+    t1 = DialogCloseThread()
     t1.start()
     qtbot.mouseClick(mainwin.clrBtnDict["text"], Qt.LeftButton)
-    t1.join()
+    t1.wait()
     qapp.processEvents()
     qtbot.wait(200)
     assert mainwin.clrBtnDict["text"].text() == "#222222"
