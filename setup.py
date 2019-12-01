14d1ab1d18e104f5a613bfaf4fe62af24e2d3157
hustlei <hustlei@sina.cn>
2019-12-01 20:37:09 +0800

    add pypi uplaod setting, fixed version err and license err in setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,22 @@
 
 from setuptools import setup, find_packages
 
+def myversion():
+    from setuptools_scm.version import guess_next_version
+    def local_scheme(version):
+        return version.format_choice(".r{distance}", ".a{distance}")
+        # return version.format_choice(".d{time:%Y%m%d}", ".{distance}.{node}.{time:%Y%m%d}")
+        # return ".dev{}".format(version.distance)
+    def version_scheme(version):
+        return guess_next_version(version.tag)
+
+    return {'version_scheme': version_scheme,'local_scheme': local_scheme}
+
 setup(
     name="preimport",
     setup_requires=['setuptools_scm'],
     # version="1.0",
-    use_scm_version=True,
+    use_scm_version=myversion,
     python_requires='>=2.7.*',
     # src_root = ".",
     package_dir={'': '.'},
@@ -23,7 +34,7 @@ setup(
     url="https://github.com/hustlei/preimport",
     project_urls={"Source Code": "https://github.com/hustlei/preimport"},
     classifiers=[
-        "License :: OSI Approved :: GNU LESSER GENERAL PUBLIC LICENSE v2.1 (LGPLv2.1)",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
         "Programming Language :: Python", "Operating System :: OS Independent"
     ])
 
