2b4a6d3380754138f39c415467d9e7faa7c5f39a
hustlei <hustlei@sina.cn>
2019-12-25 22:14:08 +0800

    update settings

--- CodeEditor/settings.py
+++ CodeEditor/settings.py
@@ -7,7 +7,8 @@ Copyright (c) 2019 lileilei <hustlei@sina.cn>
 """
 
 import os
-from PyQt5.QtCore import Qt, QVariant
+from PyQt5.QtCore import Qt, QVariant, QCoreApplication
+from PyQt5.QtGui import QColor
 from PyQt5.QtWidgets import (QWidget, QGroupBox, QVBoxLayout, QPushButton, QLabel, QHBoxLayout, QSpinBox, QCheckBox,
                              QComboBox, QColorDialog)
 from PyQt5.Qsci import QsciScintilla
@@ -15,206 +16,214 @@ from setting_enums import EnumError, SettingEnums
 from lang import language_extensions
 
 
-settingItems = {
-    # Boolean settings
-    'tabIndents': {
-        'label': 'Tab indents',
-        'type': 'bool',
-        'help': 'Use the tab key to indent text',
-    },
-    'backspaceUnindents': {
-        'label': 'Backspace unindents',
-        'type': 'bool',
-        'help': 'Backspace will unindent a line instead of just deleting a character',
-    },
-    'autoIndent': {
-        'label': 'Auto-indent',
-        'type': 'bool',
-        'help': 'Automatically indent text to match the preceding line',
-    },
-    'indentationGuides': {
-        'label': 'Indentation guides',
-        'type': 'bool',
-        'help': 'Display visible guidelines to help keep indentation consistent',
-    },
-    'indentationsUseTabs': {
-        'label': 'Use tab character',
-        'type': 'bool',
-        'help': 'Tab key inserts an actual tab character instead of spaces',
-    },
-    'eolVisibility': {
-        'label': 'Show CR/LF',
-        'type': 'bool',
-        'help': 'Display a visible icon for carriage return and line feeds',
-    },
-
-    # Color settings
-    'color': {
-        'label': 'Text color',
-        'type': 'color',
-    },
-    'paper': {
-        'label': 'Paper color',
-        'type': 'color',
-    },
-
-    # Numeric settings
-    'edgeColumn': {
-        'label': 'Text width',
-        'type': 'number',
-        'help': 'Number of characters per line before wrapping occurs',
-    },
-    'tabWidth': {
-        'label': 'Tab width',
-        'type': 'number',
-        'help': 'Width of tabs in characters, or the number of'
-                ' spaces to insert when tab is pressed',
-    },
-
-    # Multiple-selection settings
-    'braceMatching': {
-        'label': 'Brace Matching',
-        'type': 'combo',
-        'help': 'Whether and how to highlight matching {} [] () braces',
-        'valuetype': QsciScintilla.BraceMatch,
-    },
-    'edgeMode': {
-        'label': 'Edge Mode',
-        'type': 'combo',
-        'help': 'How the edge of the text width is indicated',
-        'valuetype': QsciScintilla.EdgeMode,
-    },
-    'eolMode': {
-        'label': 'Line Endings',
-        'type': 'combo',
-        'help': 'End lines with carriage return and/or line feed',
-        'valuetype': QsciScintilla.EolMode,
-    },
-    'folding': {
-        'label':
-            'Folding',
-        'type':
-            'combo',
-        'help':
-            'What kind of icons to display for code-folding',
-        'valuetype': QsciScintilla.FoldStyle
-    },
-    'whitespaceVisibility': {
-        'label':
-            'Whitespace',
-        'type':
-            'combo',
-        'help':
-            'Whether whitespace is indicated with visible markers',
-        'valuetype': QsciScintilla.WhitespaceVisibility
-    },
-    'wrapMode': {
-        'label': 'Wrap Mode',
-        'type': 'combo',
-        'help': 'How to wrap text when it reaches the text width',
-        'valuetype': QsciScintilla.WrapMode
-    },
-    'language': {
-        'label': 'Language',
-        'type': 'combo',
-        'help': 'Syntax highlighting language',
-        'valuetype': 'language',
-    },
-
-    # TODO: Need a getter for this
-    # 'caretWidth': {
-    # 'label': 'Caret width (pixels)',
-    # },
-
-    # Stuff the user probably doesn't care about configuring
-    # (or do you?)
-    # 'annotationDisplay': {
-    # 'label': 'Annotation Display',
-    # 'type': 'combo',
-    # 'values': (
-    # ('Hidden', 'AnnotationHidden'),
-    # ('Standard', 'AnnotationStandard'),
-    # ('Boxed', 'AnnotationBoxed'),
-    # ),
-    # },
-    # 'autoCompletionSource': {
-    # 'label': 'Auto Completion Source',
-    # 'type': 'combo',
-    # 'values': (
-    # ('None', 'AcsNone'),
-    # ('All', 'AcsAll'),
-    # ('Document', 'AcsDocument'),
-    # ('APIs', 'AcsAPIs'),
-    # ),
-    # },
-    # 'callTipsStyle': {
-    # 'label': 'Call Tips Style',
-    # 'type': 'combo',
-    # 'values': (
-    # ('None', 'CallTipsNone'),
-    # ('No Context', 'CallTipsNoContext'),
-    # ('No Auto-completion Context', 'CallTipsNoAutoCompletionContext'),
-    # ('Context', 'CallTipsContext'),
-    # ),
-    # },
-}
-
-# Setting groups
-_settingGroups = {
-    'Colors': {'color', 'paper', },
-    'Indentation': {'tabWidth', 'tabIndents', 'backspaceUnindents', 'autoIndent', 'indentationGuides',
-                    'indentationsUseTabs', },
-    'Wrapping': {'edgeMode', 'wrapMode', 'edgeColumn', },
-    'Formatting': {'whitespaceVisibility', 'eolMode', 'eolVisibility', },
-    'Coding aids': {'language', 'folding', 'braceMatching', },
-}
-
-# Write-only color settings.
-# FIXME: Can't effectively include these until getters are written.
-_other_color_settings = (
-    # Selection
-    'selectionForegroundColor',
-    'selectionBackgroundColor',
-    # Caret (current line)
-    'caretForegroundColor',
-    'caretLineBackgroundColor',
-    # Edge marker
-    'edgeColor',
-    # Indentation guides
-    'indentationGuidesForegroundColor',
-    'indentationGuidesBackgroundColor',
-    # Brace matching
-    'matchedBraceForegroundColor',
-    'matchedBraceBackgroundColor',
-    'unmatchedBraceForegroundColor',
-    'unmatchedBraceBackgroundColor',
-    # Marker colors
-    'markerForegroundColor',
-    'markerBackgroundColor',
-    # Margins
-    'marginsForegroundColor',
-    'marginsBackgroundColor',
-    # CallTips
-    'callTipsForegroundColor',
-    'callTipsBackgroundColor',
-    'callTipsHighlightColor',
-)
-
 class EditorSettings():
     """A dialog window for configuring a QsciScintilla editor."""
+    settingItems = {
+        # Boolean settings
+        'tabIndents': {
+            'type': 'bool',
+            'label': QCoreApplication.translate('EditorSettings', 'Tab indents'),
+            'help': QCoreApplication.translate('EditorSettings', 'Use the tab key to indent text'),
+        },
+        'backspaceUnindents': {
+            'type': 'bool',
+            'label': QCoreApplication.translate('EditorSettings', 'Backspace unindents'),
+            'help': QCoreApplication.translate('EditorSettings',
+                                               'Backspace will unindent a line instead of just deleting a character'),
+        },
+        'autoIndent': {
+            'type': 'bool',
+            'label': QCoreApplication.translate('EditorSettings', 'Auto-indent'),
+            'help': QCoreApplication.translate('EditorSettings',
+                                               'Automatically indent text to match the preceding line'),
+        },
+        'indentationGuides': {
+            'type': 'bool',
+            'label': QCoreApplication.translate('EditorSettings', 'Indentation guides'),
+            'help': QCoreApplication.translate('EditorSettings',
+                                               'Display visible guidelines to help keep indentation consistent'),
+        },
+        'indentationsUseTabs': {
+            'type': 'bool',
+            'label': QCoreApplication.translate('EditorSettings', 'Use tab character'),
+            'help': QCoreApplication.translate('EditorSettings',
+                                               'Tab key inserts an actual tab character instead of spaces'),
+        },
+        'eolVisibility': {
+            'type': 'bool',
+            'label': QCoreApplication.translate('EditorSettings', 'Show CR/LF'),
+            'help': QCoreApplication.translate('EditorSettings',
+                                               'Display a visible icon for carriage return and line feeds'),
+        },
+
+        # Color settings
+        'color': {
+            'type': 'color',
+            'label': QCoreApplication.translate('EditorSettings', 'Text color'),
+            'help': QCoreApplication.translate('EditorSettings',
+                                               'Default text color'),
+        },
+        'paper': {
+            'type': 'color',
+            'label': QCoreApplication.translate('EditorSettings', 'Paper color'),
+            'help': QCoreApplication.translate('EditorSettings',
+                                               'Default background color'),
+        },
+
+        # Numeric settings
+        'edgeColumn': {
+            'type': 'number',
+            'label': QCoreApplication.translate('EditorSettings', 'Text width'),
+            'help': QCoreApplication.translate('EditorSettings',
+                                               'Number of characters per line before wrapping occurs'),
+        },
+        'tabWidth': {
+            'type': 'number',
+            'label': QCoreApplication.translate('EditorSettings', 'Tab width'),
+            'help': QCoreApplication.translate('EditorSettings',
+                                               'Width of tabs in characters, or the number of spaces to insert when tab is pressed'),
+        },
+
+        # Multiple-selection settings
+        'braceMatching': {
+            'type': 'combo',
+            'valuetype': QsciScintilla.BraceMatch,
+            'label': QCoreApplication.translate('EditorSettings', 'Brace Matching'),
+            'help': QCoreApplication.translate('EditorSettings',
+                                               'Whether and how to highlight matching {} [] () braces'),
+        },
+        'edgeMode': {
+            'type': 'combo',
+            'valuetype': QsciScintilla.EdgeMode,
+            'label': QCoreApplication.translate('EditorSettings', 'Edge Mode'),
+            'help': QCoreApplication.translate('EditorSettings', 'How the edge of the text width is indicated'),
+        },
+        'eolMode': {
+            'type': 'combo',
+            'valuetype': QsciScintilla.EolMode,
+            'label': QCoreApplication.translate('EditorSettings', 'Line Endings'),
+            'help': QCoreApplication.translate('EditorSettings', 'End lines with carriage return and/or line feed'),
+        },
+        'folding': {
+            'type': 'combo',
+            'valuetype': QsciScintilla.FoldStyle,
+            'label': QCoreApplication.translate('EditorSettings', 'Folding'),
+            'help': QCoreApplication.translate('EditorSettings', 'What kind of icons to display for code-folding')
+        },
+        'whitespaceVisibility': {
+            'type': 'combo',
+            'valuetype': QsciScintilla.WhitespaceVisibility,
+            'label': QCoreApplication.translate('EditorSettings', 'Whitespace'),
+            'help': QCoreApplication.translate('EditorSettings', 'Whether whitespace is indicated with visible markers')
+        },
+        'wrapMode': {
+            'type': 'combo',
+            'valuetype': QsciScintilla.WrapMode,
+            'label': QCoreApplication.translate('EditorSettings', 'Wrap Mode'),
+            'help': QCoreApplication.translate('EditorSettings', 'How to wrap text when it reaches the text width')
+        },
+        'language': {
+            'type': 'combo',
+            'valuetype': 'language',
+            'label': QCoreApplication.translate('EditorSettings', 'Language'),
+            'help': QCoreApplication.translate('EditorSettings', 'Syntax highlighting language'),
+        },
+
+        # TODO: Need a getter for this
+        # 'caretWidth': {
+        # 'label': 'Caret width (pixels)',
+        # },
+
+        # Stuff the user probably doesn't care about configuring
+        # (or do you?)
+        # 'annotationDisplay': {
+        # 'label': 'Annotation Display',
+        # 'type': 'combo',
+        # 'values': (
+        # ('Hidden', 'AnnotationHidden'),
+        # ('Standard', 'AnnotationStandard'),
+        # ('Boxed', 'AnnotationBoxed'),
+        # ),
+        # },
+        # 'autoCompletionSource': {
+        # 'label': 'Auto Completion Source',
+        # 'type': 'combo',
+        # 'values': (
+        # ('None', 'AcsNone'),
+        # ('All', 'AcsAll'),
+        # ('Document', 'AcsDocument'),
+        # ('APIs', 'AcsAPIs'),
+        # ),
+        # },
+        # 'callTipsStyle': {
+        # 'label': 'Call Tips Style',
+        # 'type': 'combo',
+        # 'values': (
+        # ('None', 'CallTipsNone'),
+        # ('No Context', 'CallTipsNoContext'),
+        # ('No Auto-completion Context', 'CallTipsNoAutoCompletionContext'),
+        # ('Context', 'CallTipsContext'),
+        # ),
+        # },
+    }
+
+    # Setting groups
+    settingGroups = {
+        'Colors': {'label': QCoreApplication.translate('EditorSettings', 'Colors'),
+                   'items': {'color', 'paper', }},
+        'Indentation': {'label': QCoreApplication.translate('EditorSettings', 'Indentation'),
+                        'items': {'tabWidth', 'tabIndents', 'backspaceUnindents', 'autoIndent', 'indentationGuides',
+                                  'indentationsUseTabs', }},
+        'Wrapping': {'label': QCoreApplication.translate('EditorSettings', 'Wrapping'),
+                     'items': {'edgeMode', 'wrapMode', 'edgeColumn', }},
+        'Formatting': {'label': QCoreApplication.translate('EditorSettings', 'Formatting'),
+                       'items': {'whitespaceVisibility', 'eolMode', 'eolVisibility', }},
+        'Coding aids': {'label': QCoreApplication.translate('EditorSettings', 'Coding aids'),
+                        'items': {'language', 'folding', 'braceMatching', }},
+    }
+
+    # Write-only color settings.
+    # FIXME: Can't effectively include these until getters are written.
+    _other_color_settings = (
+        # Selection
+        'selectionForegroundColor',
+        'selectionBackgroundColor',
+        # Caret (current line)
+        'caretForegroundColor',
+        'caretLineBackgroundColor',
+        # Edge marker
+        'edgeColor',
+        # Indentation guides
+        'indentationGuidesForegroundColor',
+        'indentationGuidesBackgroundColor',
+        # Brace matching
+        'matchedBraceForegroundColor',
+        'matchedBraceBackgroundColor',
+        'unmatchedBraceForegroundColor',
+        'unmatchedBraceBackgroundColor',
+        # Marker colors
+        'markerForegroundColor',
+        'markerBackgroundColor',
+        # Margins
+        'marginsForegroundColor',
+        'marginsBackgroundColor',
+        # CallTips
+        'callTipsForegroundColor',
+        'callTipsBackgroundColor',
+        'callTipsHighlightColor',
+    )
 
     def __init__(self, editor=None):
-        self.previousSettings = {} # settings exist
-        self.nextSettings = {} # settings changed to be applied
+        self.previousSettings = {}  # settings exist
+        self.nextSettings = {}  # settings changed to be applied
         # settings in toml format (all enum using name intead)
         self.tomlDictSettings = {}
         # all setting group widgets
         self.groupWidgets = {}
         if editor:
             self.editor = editor
-            for item in settingItems:
+            for item in self.settingItems:
                 self.previousSettings[item] = self.editor.getConfig(item)
-
 
     def defaultLayout(self):
         """Create and return the main layout for the dialog widget.
@@ -237,7 +246,9 @@ class EditorSettings():
 
     def createWidgets(self):
         """Create widgets for setting and save them into self.settingWidgets, each widget is a QGroup"""
-        for label, names in _settingGroups.items():
+        for values in self.settingGroups.values():
+            label = values['label']
+            names = values['items']
             self.groupWidgets[label] = QGroupBox(label)
             group_layout = QVBoxLayout()
             for name in names:
@@ -248,7 +259,7 @@ class EditorSettings():
     def _create_widget(self, name):
         """Return an appropriate widget for the given configuration setting.
         """
-        setting = settingItems[name]
+        setting = self.settingItems[name]
         type_ = setting['type']
 
         # Get the appropriate widget type
@@ -300,10 +311,11 @@ class EditorSettings():
 
     def _create_combobox(self, name):
         """Return a combobox for modifying a multiple-getValue setting."""
-        setting = settingItems[name]
+        setting = self.settingItems[name]
         valuetype = setting['valuetype']
         # Create the combobox and populate it
         combo = QComboBox()
+        combo.setMinimumWidth(100)
         for value, valueinfo in SettingEnums.enums[valuetype].items():
             data = QVariant(value)
             combo.addItem(valueinfo["display"], data)
@@ -324,24 +336,28 @@ class EditorSettings():
         return combo
 
     def _create_color_picker(self, name):
-        """Return a color-picker widget for a color-based setting.
-        """
+        """Return a color-picker widget for a color-based setting."""
         # Button with colored background
         button = QPushButton()
+        button.setMinimumWidth(80)
+        current_color = self.previousSettings.get(name, Qt.red)
+        try:
+            current_color = current_color.name()
+        except:
+            pass
 
         # Event handler
         def button_pressed():
-            current_color = self.previousSettings.get(name, Qt.white)
-            color = QColorDialog.getColor(current_color)
-            button.setStyleSheet("background-color: %s" % color.name())
-            self.nextSettings[name] = color
+            color = QColorDialog.getColor(QColor(current_color))
+            if color.isValid():
+                button.setStyleSheet("background-color: %s" % color.name())
+                self.nextSettings[name] = color.name()
 
         # Connect event handler
         button.pressed.connect(button_pressed)
 
         # Set default background color
-        color = self.previousSettings.get(name, Qt.white)
-        button.setStyleSheet("background-color: %s" % color)
+        button.setStyleSheet("background-color: %s" % current_color)
 
         return button
 
@@ -349,6 +365,7 @@ class EditorSettings():
         """Return a numeric entry widget for a numeric setting.
         """
         spinbox = QSpinBox()
+        spinbox.setMaximumWidth(120)
 
         # Set initial getValue
         spinbox.setValue(self.previousSettings.get(name, 5))
@@ -393,7 +410,7 @@ if __name__ == "__main__":
 
     app = QApplication(sys.argv)
     win = QWidget()
-    layout=QHBoxLayout()
+    layout = QHBoxLayout()
     win.setLayout(layout)
     ed = Editor()
     d = QWidget(win)
